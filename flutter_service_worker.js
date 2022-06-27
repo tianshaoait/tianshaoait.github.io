9d209ad1ce92c4d90e1f5ddfee3308c9d102df0d
ltr123 <404566942@qq.com>
2022-06-27 21:58:53 +0800

    提交新版6

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,9 +27,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "a73f120c199eb9462260d584146bfe4e",
 "icons/Icon-maskable-192.png": "a73f120c199eb9462260d584146bfe4e",
 "icons/Icon-maskable-512.png": "a73f120c199eb9462260d584146bfe4e",
-"index.html": "b178526ec5aa47ecd1c78018389647fe",
-"/": "b178526ec5aa47ecd1c78018389647fe",
-"main.dart.js": "7f2e52fbf879dd40b24ee14d7308408a",
+"index.html": "46838faee021e77341e0b43fcd7c4f9b",
+"/": "46838faee021e77341e0b43fcd7c4f9b",
+"main.dart.js": "4ca8ea3576681ada2e7aa354a150b418",
 "manifest.json": "00b17c70d38c87f8973c274f7f59be8b",
 "version.json": "01ac0f5fe07dd2065b3663ce2a60757c"
 };
